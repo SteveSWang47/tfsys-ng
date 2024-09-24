33f7e1f15e5e7d9f21f483df109ed02313d2e24f
SteveSWang47 <stevewang47@163.com>
2024-09-24 21:01:00 +0800

    Quaery, ordered by name, backend.

---
+++ cgi/qryallbyname.cpp
@@ -0,0 +1,29 @@
+#include <mysql++/mysql++.h>
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+    printf("Content-type: text/html\n\n");
+    mysqlpp::Connection conn("tfsys");
+    mysqlpp::Query qry = conn.query("select from_unixtime(time), id, (select name from users where id = recs.id), (select class from users where id = recs.id), lpad(setid, 5, 0), (select name from sets where id = recs.setid), cast((result * 100 / (select full from sets where id = recs.setid)) as unsigned) from recs order by time desc;");
+    if(mysqlpp::StoreQueryResult res = qry.store())
+    {
+        std::cout << "<table>\n<thread><tr><th>时间</th><th>学工号</th><th>姓名</th><th>班级</th><th>题组编号</th><th>题组名称</th><th>得分</th></tr></thread>\n<tbody>";
+        for(auto rw : res)
+        {
+            std::cout << "<tr>\n";
+            for(auto cl : rw)
+            {
+                std::cout << "<td>" << cl << "</td>\n";
+            }
+            std::cout << "</tr>\n";
+        }
+        std::cout << "</table><br />\n";
+    }
+    else
+    {
+        std::cout << "ERROR: " << qry.error() << "请报告此问题。\n";
+    }
+    return 0;
+}
