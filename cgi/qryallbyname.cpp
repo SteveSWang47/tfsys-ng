cf3d9b8ddf85d1e6bea1a928785ea8d896ba285d
SteveSWang47 <stevewang47@163.com>
2024-09-26 17:01:02 +0800

    Qryallbyname order change

--- cgi/qryallbyname.cpp
+++ cgi/qryallbyname.cpp
@@ -6,7 +6,7 @@ int main()
 {
     printf("Content-type: text/html\n\n");
     mysqlpp::Connection conn("tfsys");
-    mysqlpp::Query qry = conn.query("select from_unixtime(time), id, (select name from users where id = recs.id), (select class from users where id = recs.id), lpad(setid, 5, 0), (select name from sets where id = recs.setid), cast((result * 100 / (select full from sets where id = recs.setid)) as unsigned) from recs order by time desc;");
+    mysqlpp::Query qry = conn.query("select from_unixtime(time), id, (select name from users where id = recs.id), (select class from users where id = recs.id), lpad(setid, 5, 0), (select name from sets where id = recs.setid), cast((result * 100 / (select full from sets where id = recs.setid)) as unsigned) from recs order by name, setid, time asc;");
     if(mysqlpp::StoreQueryResult res = qry.store())
     {
         std::cout << "<table>\n<thread><tr><th>时间</th><th>学工号</th><th>姓名</th><th>班级</th><th>题组编号</th><th>题组名称</th><th>得分</th></tr></thread>\n<tbody>";
